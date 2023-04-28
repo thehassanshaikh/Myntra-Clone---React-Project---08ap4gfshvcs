4b750ca47b3d196f8285f2c1dca3e9d3fede3c83
thehassanshaikh <thehasanshaikh@gmail.com>
2023-04-28 17:52:51 +0530

    updated home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -125,6 +125,155 @@ function Home() {
                     </div>
                 </div>
             </div>
+            <div className="text-banner-container">
+                <h4 className='text-banner-title'>Categories to bag</h4>
+            </div>
+            <div className="container-container">
+                <div className="row-base">
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/aa4658b5-d723-4652-9ea1-00456b355c3a1645602467046-Kurta-Sets.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/aae4be67-e611-47f4-b94e-92a16a36df731645602467007-Hangbags.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/0f0be09e-4155-47bf-82e1-51044e7e7fd11645602467052-Kurtas.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/dd4414f8-4e1b-4a22-997e-8e06c0a5ff861645602467167-T-Shirts.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/f9ca5609-b634-42d4-8c08-a8eaebb818b71645602467085-Sarees.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/0b7869d4-f825-4625-b1db-58ad10a45f301645602467093-Shirts.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/b34a30a6-504b-4c94-b7e1-61391d536bc51645602467038-Jewellery.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/b156f76a-26e7-4bce-9941-8a67d3c16f331645602467120-Teens-Wear.jpg" alt="" />
+                    </div>
+                </div>
+                <div className="row-base">
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/fb091b07-c275-4578-b08d-b4f93dfe9e841645602466976-Beauty.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/b827f900-ed61-4467-84fa-a6e357787e761645602467079-Plus-Size-Styles_W.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/cd083042-3bb2-4231-8b96-0234fc0ed23f1645602467032-Jeans.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/736f3951-e67b-414f-bfb1-56e2794d441d1645602467114-Sports-Shoes.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/d13255df-c846-4dbd-8458-77ccaba4f9eb1645602467142-Trousers.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/7a774194-94e6-49b5-b8bb-64bf9901bc671645602466989-Casual-Shoes.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/5860c3c2-a639-4625-ac1d-4d55406f128a1645602467134-Track-Pants.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/07e5c29a-2eab-4b2d-b617-6565ffe1f4701645602467025-Innerwear.jpg" alt="" />
+                    </div>
+                </div>
+                <div className="row-base">
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/09b3164c-241a-4134-baa1-49b12c56c3901645602466968-Bath-Essentials.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/171a820e-96f0-4b11-a138-03953a7e05481645602467153-Trousers_W.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/054a056f-33e8-4b6c-b747-9b88d7fce0a11645602467174-Watches.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/d3c5cd23-392f-40be-8080-99ffb79c27261645602467128-Tops.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/355b9499-3345-4457-8b3b-2eeceaecf4561645602467020-Infant-Essentials.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/7f814546-b705-4d9b-9ab5-1ddfeca786391645602467001-Flip-Flops.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/12c3b4aa-8160-442f-b93e-e896eafb1b0a1645602467160-T-Shirts-_-Shorts_Kids.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2022/2/23/67783047-7fcc-4530-9368-9be75a713e411645602466995-Dresses.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+ {/*================= DEALS ON TOP BRANDS ==========================*/}
+            <div className="text-banner-container">
+                <h4 className='text-banner-title'>DEALS ON TOP BRANDS</h4>
+            </div>
+            <div className="container-container">
+                <div className="row-base">
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/retaillabs/2020/9/3/f0dc0024-a8d3-4aec-9e1b-d9b6873b52d51599144996663-Crocs.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/85d5e2e1-fa21-43b0-a352-438e15f1006e1598892141455-Nike.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/22b908fa-38aa-46a6-a3cd-a5ccd46b04ce1598892141560-Puma.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/retaillabs/2020/9/26/adc58a2c-fa18-4b94-b2d6-4c692a44123d1601115417606-unnamed--6-.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/8238147e-a86e-4fe4-a830-ab5c2c49beba1598892141840-W.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/retaillabs/2020/9/1/7d0153c1-2fc4-49ec-ac7c-3c402d1ed45f1598948396008-USPA.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/c6a4ec29-f907-4d54-8ed7-83006938b20c1598892141612-Tommy-Hilfiger.jpg" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="https://assets.myntassets.com/f_webp,w_122,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/26/d1fab403-7742-4a91-a781-2e36bea65a8a1598445950683-Men-s-Apparel---Roadster.png" alt="" />
+                    </div>
+                </div>
+            </div>
+
+            {/*=================== BRANDS AT SLASHED PRICES  ================= */}
+
+            <div className="text-banner-container">
+                <h4 className='text-banner-title'>BRANDS AT SLASHED PRICES</h4>
+            </div>
+            <div className="container-container">
+                <div className="row-base">
+                    <div className="coloum-base">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="coloum-base">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }

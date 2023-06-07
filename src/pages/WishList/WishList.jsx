82fb4f8769cab70c17945bf9f310278503a74866
thehassanshaikh <thehasanshaikh@gmail.com>
2023-06-07 17:41:48 +0530

    added product page style on wishlist page

--- src/pages/WishList/WishList.jsx
+++ src/pages/WishList/WishList.jsx
@@ -1,5 +1,6 @@
 import { ApiContext } from "../../components/Context/ApiContext";
 import { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./WishList.css";
 
 export const WishList = () => {
@@ -14,24 +15,27 @@ export const WishList = () => {
 
     return (
         <>
-        <h2 className="wish-heading">My Wishlist</h2>
-            <div className="wishlist-container">
-                
-                {
-                    wishlist.map(item => (
-                        <div className="wish-product-con">
-                            <img src={item.image} alt="" srcset="" />
-                            <div className="wish-title">
-                                <h4>{item.title}</h4>
+        <section className="wishList-section">
+            <h2 className="wish-heading">My Wishlist</h2>
+           {wishlist.lenght > 0 ? console.log("true") : console.log("false")}
+           
+            <div className="products-container">
+                    {wishlist.map(item => (
+                        <div className="product-grid">
+                            <div className="pro-img-con">
+                                <img src={item.image} alt="product image"
+                                    className="pro-img" />
                             </div>
-                            <button>Add to cart</button>
+                            <div className="product-ditails">
+                                <h3 className="product-brand">{item.category}</h3>
+                                <h3 className="product-title">{item.title}</h3>
+                                <h3 className="product-price">₹ {item.price}</h3>
+                                <Link to={`/product-details/${item.id}`} key={item.id}> <button className="product-button"><a href="#">View More</a></button></Link>
+                            </div>
                         </div>
-
-                    ))
-                }
-
-            </div>
-
+                    ))}
+                </div>
+        </section>
         </>
     )
 }
